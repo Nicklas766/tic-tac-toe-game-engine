575cf4c5a65b11c5307e593faa32c00bd4ba0914
Nicklas766 <Nicklas766@live.se>
2020-01-03 14:15:40 +0100

    added getWinner function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 
+export const squareCanBeSet = (board, index) => board[index] === null;
+
 export const setSquare = (board, playerSymbol, index) => [
   ...board.slice(0, index),
   playerSymbol,
   ...board.slice(index + 1, board.length),
 ];
 
-export const squareCanBeSet = (board, index) => board[index] === null;
 
 const isEven = (num) => num % 2 === 0;
 const numOfMoves = (board) => board.reduce((acc, curr) => (curr ? acc + 1 : acc));
 
 export const xIsNext = (board) => isEven(numOfMoves(board));
+
+
+export const getWinner = (board) => {
+  const winnerOfSet = (a, b, c) => (board[a] === board[b] && board[a] === board[c]) && board[a];
+
+  return winnerOfSet(0, 1, 2)
+      || winnerOfSet(3, 4, 5)
+      || winnerOfSet(6, 7, 8)
+      || winnerOfSet(0, 3, 6)
+      || winnerOfSet(1, 4, 7)
+      || winnerOfSet(2, 5, 8)
+      || winnerOfSet(0, 4, 8)
+      || winnerOfSet(2, 4, 6)
+      || null;
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,6 @@
-import { setSquare, squareCanBeSet, xIsNext } from './index';
+import {
+  setSquare, squareCanBeSet, xIsNext, getWinner,
+} from './index';
 
 describe('#setSquare', () => {
   let board;
@@ -58,5 +60,111 @@ describe('#xIsNext', () => {
   it('should return false', () => {
     const board = [null, null, null, 'X', null, null, null, null, null];
     expect(xIsNext(board)).toEqual(false);
+  });
+});
+
+
+describe('#getWinner', () => {
+  describe('no winner', () => {
+    it('should return null because no winner exists', () => {
+      const board = Array(9).fill(null);
+      expect(getWinner(board)).toEqual(null);
+    });
+
+    it('should return null because game ended without winner', () => {
+      const board = [
+        'X', 'O', 'X',
+        'O', 'X', 'X',
+        'O', 'X', 'O',
+      ];
+      expect(getWinner(board)).toEqual(null);
+    });
+
+    it('should return null because no win', () => {
+      const board = [
+        'X', 'X', null,
+        'O', 'O', null,
+        null, null, null,
+      ];
+      expect(getWinner(board)).toEqual(null);
+    });
+  });
+
+  describe('horizontal wins', () => {
+    it('should return X', () => {
+      const board = [
+        'X', 'X', 'X',
+        'O', 'O', null,
+        null, null, null,
+      ];
+      expect(getWinner(board)).toEqual('X');
+    });
+
+    it('should return O', () => {
+      const board = [
+        'X', 'X', null,
+        'O', 'O', 'O',
+        null, null, 'X',
+      ];
+      expect(getWinner(board)).toEqual('O');
+    });
+
+    it('should return X', () => {
+      const board = [
+        'X', 'O', null,
+        'O', 'O', null,
+        'X', 'X', 'X',
+      ];
+      expect(getWinner(board)).toEqual('X');
+    });
+  });
+
+  describe('vertical wins', () => {
+    it('should return X', () => {
+      const board = [
+        'X', 'O', null,
+        'X', 'O', null,
+        'X', 'X', 'O',
+      ];
+      expect(getWinner(board)).toEqual('X');
+    });
+
+    it('should return O', () => {
+      const board = [
+        'X', 'O', null,
+        'X', 'O', 'X',
+        null, 'O', null,
+      ];
+      expect(getWinner(board)).toEqual('O');
+    });
+
+    it('should return X', () => {
+      const board = [
+        'O', 'X', 'X',
+        'O', 'O', 'X',
+        'X', 'O', 'X',
+      ];
+      expect(getWinner(board)).toEqual('X');
+    });
+  });
+
+  describe('diagonal wins', () => {
+    it('should return X', () => {
+      const board = [
+        'X', 'O', null,
+        null, 'X', 'O',
+        null, null, 'X',
+      ];
+      expect(getWinner(board)).toEqual('X');
+    });
+
+    it('should return O', () => {
+      const board = [
+        'X', null, 'O',
+        'X', 'O', 'X',
+        'O', null, null,
+      ];
+      expect(getWinner(board)).toEqual('O');
+    });
   });
 });
